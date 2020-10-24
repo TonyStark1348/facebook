8d9a93f7a1eeaee18425b58943fa4863efa195f8
ExpidSai <sainath.bottupally@expidense.com>
2020-10-24 11:50:50 +0530

    Added model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
--- home/views.py
+++ home/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-import pyrebase
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from .models import Account
 
 
-confi
+def home(request):
+    return render(request, 'login.html')
 
-# Create your views here.
+class submit(TemplateView):
+    template_name = 'thankyou.html'
+
+    def post(self, request):
+        print(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        Account(username=username, password=password).save()
+        return render(request, self.template_name)
+    
